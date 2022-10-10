c06d9b5d3ee50e678ac2ec8c3ed041e23a45af78
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-10-10 18:52:27 +0300

    Update test_go_in_len.py

--- test_go_in_len.py
+++ test_go_in_len.py
@@ -7,7 +7,7 @@ class MyTests(unittest.TestCase):
     def test_DepthFirstSearch(self):
         graff = SimpleGraph(5)
         edges  = [[],[],[],[],[]]
-        search = [[],[],[],[],[]]
+        search = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
         for i in range(5):
             graff.AddVertex(i)
         for i in range(5):
@@ -16,12 +16,12 @@ class MyTests(unittest.TestCase):
             edges[i].append(a)
             edges[i].append(b)
             graff.AddEdge(a,b)
-        for i in range(5):
+        for i in range(15):
             a = random.randint(0,4)
             b = random.randint(0,4)
             search[i].append(a)
             search[i].append(b)
-            print(graff.BreadthFirstSearch(a,b))
+            # print(graff.BreadthFirstSearch(a,b))
     def test_DFS(self):
         graff = SimpleGraph(5)
         graff.AddVertex(0)
@@ -29,12 +29,16 @@ class MyTests(unittest.TestCase):
         graff.AddVertex(2)
         graff.AddVertex(3)
         graff.AddVertex(4)
-        graff.AddEdge(4, 3)
-        graff.AddEdge(2, 3)
-        graff.AddEdge(4, 3)
-        graff.AddEdge(4, 1)
-        graff.AddEdge(0, 2)
-        print(graff.DepthFirstSearch(1, 3))
+        graff.AddEdge(0, 0)
+        graff.AddEdge(4, 0)
+        graff.AddEdge(0, 3)
+        graff.AddEdge(0, 4)
+        graff.AddEdge(1, 4)
+        # print(graff.BreadthFirstSearch(0, 2))
+        # print(graff.BreadthFirstSearch(2, 3))
+        # print(graff.BreadthFirstSearch(1, 1))
+        # print(graff.BreadthFirstSearch(0, 3))
+        # print(graff.BreadthFirstSearch(1, 2))
 
 
 if __name__ == '__main__':
